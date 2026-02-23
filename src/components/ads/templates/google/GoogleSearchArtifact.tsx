5b4ccc01739d6fc405738c70bbc9f237059cf287
Josefina Guberna <josefina@insightsoftworks.io>
2026-02-23 15:44:59 -0300

    fix google add content

--- src/components/ads/templates/google/GoogleSearchArtifact.tsx
+++ src/components/ads/templates/google/GoogleSearchArtifact.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { useArtifact } from "@/components/ads/hooks/useArtifact";
-import GoogleSearchAd from "./GoogleSearchAd";
+import GoogleSearchAd, { type GoogleSearchAdProps } from "./GoogleSearchAd";
 
 export const GoogleSearchArtifact = () => {
   const { content, name } = useArtifact();
   return (
-    <GoogleSearchAd name={name} type="ad-template:google-search-ad" content={content} />
+    <GoogleSearchAd
+      name={name}
+      type="ad-template:google-search-ad"
+      content={content as GoogleSearchAdProps["content"]}
+    />
   );
 };
